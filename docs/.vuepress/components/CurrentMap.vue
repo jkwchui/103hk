d4ceae30389de143eee31f623d31e20ffe38396d
Freedom-hi <cuhkmedane@gmail.com>
2019-08-25 20:11:29 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -35,7 +35,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48616049297_f8ce75cefc_k_d.jpg', // <- MOSTLY EDIT ONLY THIS
+            mapurl1: 'https://live.staticflickr.com/65535/48616049297_42d7941ff7_k_d.jpg', // <- MOSTLY EDIT ONLY THIS
             mapurl2: '',
             mapurl3: '',
         }
