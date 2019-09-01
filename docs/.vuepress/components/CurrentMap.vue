a2650737d05802fbcf16c7e1d1ead6bd257b262c
Freedom-hi <cuhkmedane@gmail.com>
2019-09-01 15:55:20 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -31,7 +31,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48656832433_aaf8aa3e03_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
+            mapurl1: 'https://live.staticflickr.com/65535/48656832433_d1221bfe02_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
             mapurl2: 'https://live.staticflickr.com/65535/48657572227_5cc4da42d0_k_d.jpg', // inside terminal
         }
     }
