cfe20ee9a40d4855a94ea04dfffa2aff82eb93fa
Jon Chui <jon@lpcuwc.edu.hk>
2019-09-08 15:19:20 +0800

    9.08 initial
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -6,15 +6,15 @@
         </p>
 
         <center>
-            <a target="_blank" :href="mapurl2">
+            <!-- <a target="_blank" :href="mapurl2">
                 <img :src="mapurl2" />
-            </a>
+            </a> -->
 
             <!-- Zone 3 -->
 
-            <a target="_blank" :href="mapurl3">
+            <!-- <a target="_blank" :href="mapurl3">
                 <img :src="mapurl3" />
-            </a>
+            </a> -->
 
             <!-- Zone 2 - UNCOMMENT AS NEEDED -->
 
@@ -31,7 +31,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48656832433_c5102d388f_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
+            mapurl1: 'https://live.staticflickr.com/65535/48697488216_371a87b262_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
             mapurl2: 'https://live.staticflickr.com/65535/48657572227_95f014c655_k_d.jpg', // inside terminal
         }
     }
