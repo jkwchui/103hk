e2e896c1dbc037874199f2719e7087147f4099f5
Jon Chui <jon@lpcuwc.edu.hk>
2019-09-08 17:49:18 +0800

    1749 MTR
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -6,9 +6,7 @@
         </p>
 
         <center>
-            <!-- <a target="_blank" :href="mapurl2">
-                <img :src="mapurl2" />
-            </a> -->
+
 
             <!-- Zone 3 -->
 
@@ -22,6 +20,9 @@
                 <img :src="mapurl1" />
             </a>
             
+            <a target="_blank" :href="mapurl2">
+                <img :src="mapurl2" />
+            </a>
         </center>
     </div>
 </template>
@@ -32,7 +33,7 @@ export default {
     data () {
         return {
             mapurl1: 'https://live.staticflickr.com/65535/48697488216_9fab0bbeab_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
-            mapurl2: 'https://live.staticflickr.com/65535/48657572227_95f014c655_k_d.jpg', // inside terminal
+            mapurl2: 'https://live.staticflickr.com/65535/48698238427_513cc89ea7_k_d.jpg', // MTR
         }
     }
 }
