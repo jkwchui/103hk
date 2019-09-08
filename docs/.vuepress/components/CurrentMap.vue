a5e09e256a37aff02806d3e4e3bba833d6a99729
Jon Chui <jon@lpcuwc.edu.hk>
2019-09-08 16:26:05 +0800

    1624
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -31,7 +31,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48697488216_3649b2001e_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
+            mapurl1: 'https://live.staticflickr.com/65535/48697488216_46a88116d9_k_d.jpg', // <- MOSTLY EDIT ONLY  airport
             mapurl2: 'https://live.staticflickr.com/65535/48657572227_95f014c655_k_d.jpg', // inside terminal
         }
     }
