1d6fded8088567e6ac5641bab2cc5b94cc82f5ca
Jon Chui <jon@lpcuwc.edu.hk>
2019-09-15 17:30:20 +0800

    17:30 Adm
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -10,15 +10,15 @@
 
             <!-- Zone 3 -->
 
-
+            <a target="_blank" :href="mapurl2">
+                <img :src="mapurl2" />
+            </a>
 
             <a target="_blank" :href="mapurl1">
                 <img :src="mapurl1" />
             </a>
             
-            <a target="_blank" :href="mapurl2">
-                <img :src="mapurl2" />
-            </a>
+            
 
             <a target="_blank" :href="mapurl3">
                 <img :src="mapurl3" />
@@ -33,7 +33,7 @@ export default {
     data () {
         return {
             mapurl1: 'https://live.staticflickr.com/65535/48735026793_70716a1129_k_d.jpg', // <- CWB
-            mapurl2: 'https://live.staticflickr.com/65535/48735565191_0ecb7f198c_k_d.jpg', // <- Adm
+            mapurl2: 'https://live.staticflickr.com/65535/48735565191_ea8c4b04fb_k_d.jpg', // <- Adm
             mapurl3: 'https://live.staticflickr.com/65535/48735027008_ffd8b55f1e_k_d.jpg', // MTR
         }
     }
