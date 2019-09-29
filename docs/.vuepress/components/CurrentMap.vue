6841fa1c581d203a5a410e1e45441639ac9cc076
Jon Chui <jon@lpcuwc.edu.hk>
2019-09-29 15:19:40 +0800

    9.29 Island INIT Adm
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -10,9 +10,9 @@
 
             <!-- Zone 3 -->
 
-            <a target="_blank" :href="mapurl2">
+            <!-- <a target="_blank" :href="mapurl2">
                 <img :src="mapurl2" />
-            </a>
+            </a> -->
 
             <a target="_blank" :href="mapurl1">
                 <img :src="mapurl1" />
@@ -20,9 +20,9 @@
             
             
 
-            <a target="_blank" :href="mapurl3">
+            <!-- <a target="_blank" :href="mapurl3">
                 <img :src="mapurl3" />
-            </a>
+            </a> -->
         </center>
     </div>
 </template>
@@ -32,7 +32,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48768013196_a1f22113af_k_d.jpg', // <- Tuen Mun
+            mapurl1: 'https://live.staticflickr.com/65535/48812371513_84b924c0bc_o_d.jpg', // <- Current
             mapurl2: 'https://live.staticflickr.com/65535/48735565191_5c337d9aa8_k_d.jpg', // <- Adm
             mapurl3: 'https://live.staticflickr.com/65535/48768021896_c4f215839a_k_d.jpg', // MTR
         }
