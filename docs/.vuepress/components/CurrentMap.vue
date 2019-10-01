1959dc2e791ebfd1df9331d6b54d45020e9e330c
Freedom-hi <cuhkmedane@gmail.com>
2019-10-01 19:28:10 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -30,7 +30,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48824575273_f747aa1d2c_k_d.jpg', // <- MK
+            mapurl1: 'https://live.staticflickr.com/65535/48824575273_7183617594_k_d.jpg', // <- SSP
             mapurl2: 'https://live.staticflickr.com/65535/48824969862_c2757d117a_k_d.jpg', // <- WTS
             mapur13: 'https://live.staticflickr.com/65535/48824575273_9704dfdd75_k_d.jpg', // <- YMT
             mapurl4: 'https://live.staticflickr.com/65535/48768021896_12684aff05_k_d.jpg', // MTR
