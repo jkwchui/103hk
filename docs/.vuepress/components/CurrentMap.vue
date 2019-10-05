8ec041cc58d04abf939ac65fc2235a0912e02327
Freedom-hi <cuhkmedane@gmail.com>
2019-10-05 13:55:31 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -30,10 +30,8 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48824575273_7183617594_k_d.jpg', // <- SSP
-            mapurl2: 'https://live.staticflickr.com/65535/48824969862_c2757d117a_k_d.jpg', // <- WTS
-            mapur13: 'https://live.staticflickr.com/65535/48824575273_9704dfdd75_k_d.jpg', // <- YMT
-            mapurl4: 'https://live.staticflickr.com/65535/48768021896_12684aff05_k_d.jpg', // MTR
+            mapurl1: 'https://live.staticflickr.com/65535/48845740767_92094d690f_k_d.jpg', // <- cwb
+            mapurl2: 'https://live.staticflickr.com/65535/48768021896_12684aff05_k_d.jpg', // MTR
         }
     }
 }
