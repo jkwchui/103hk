ca5533af34e26aaa9713a6e393c96f41155539ef
Freedom-hi <cuhkmedane@gmail.com>
2019-10-06 17:02:02 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -30,7 +30,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48845740767_ecb61fe04c_k_d.jpg', // <- kowloon
+            mapurl1: 'https://live.staticflickr.com/65535/48845740767_02e4d6a032_k_d.jpg', // <- kowloon
             mapurl2: 'https://live.staticflickr.com/65535/48768021896_12684aff05_k_d.jpg', // MTR
         }
     }
