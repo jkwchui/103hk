b9e73bdbdd9e2bebd8a93d1163db021902753692
jkwchui <jon@lpcuwc.edu.hk>
2019-10-20 18:18:08 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -42,7 +42,7 @@ export default {
     data () {
         return {
             mapurl1: 'https://live.staticflickr.com/65535/48927859477_51a4b73d81_k_d.jpg', // <- kowloon MK
-            mapurl2: 'https://live.staticflickr.com/65535/48927859477_c5384dd873_k_d.jpg', // <- TST
+            mapurl2: 'https://live.staticflickr.com/65535/48927859477_e5f3b5daa1_k_d.jpg', // <- TST
             mapurl3: 'https://live.staticflickr.com/65535/48927859477_20fc3d32e3_k_d.jpg', // <- SSP
             mapurl4: 'https://live.staticflickr.com/65535/48927859477_fd015e4215_b_d.jpg', // <- MTR
         }
