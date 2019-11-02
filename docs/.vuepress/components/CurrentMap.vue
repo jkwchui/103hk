c5808899ba10ae2b9de4c1971a47d6c0288275ec
jkwchui <jon@lpcuwc.edu.hk>
2019-11-02 17:41:09 +0800

    Update CurrentMap.vue

--- docs/.vuepress/components/CurrentMap.vue
+++ docs/.vuepress/components/CurrentMap.vue
@@ -31,7 +31,7 @@ export default {
     components: { },
     data () {
         return {
-            mapurl1: 'https://live.staticflickr.com/65535/48999017173_851d52e4b1_k_d.jpg', // <- CWB/Wanchai
+            mapurl1: 'https://live.staticflickr.com/65535/48999017173_7a93b17631_k_d.jpg', // <- CWB/Wanchai
             mapurl2: ' ', // <- Admiralty
          
             
